b8baaf9f8b9511452375c31284402a3a031eaa80
djedd1ne <58569774+djedd1ne@users.noreply.github.com>
2024-02-07 03:25:10 +0100

    add fernet.py intro to symmetric encryption

---
+++ fernet.py
@@ -0,0 +1,18 @@
+import cryptography
+
+#Use symmetric encryption
+from cryptography.fernet import Fernet
+
+#Genetate a key
+key = Fernet.generate_key()
+
+#Create a Fernet object
+cipher = Fernet(key)
+
+#The message we want to encrypt
+message = "This is a secret message".encode()
+print("Original message: ", message.decode())
+
+#Encrypt the message
+encrypted_message = cipher.encrypt(message)
+print("Secret message ", encrypted_message.decode())
