d86a3ddb067368936ff219d3b5a23251b3597f32
djedd1ne <58569774+djedd1ne@users.noreply.github.com>
2024-11-29 14:49:13 +0100

    Update fernet_receive.py

--- fernet_receive.py
+++ fernet_receive.py
@@ -1,1 +1,21 @@
+# Import cryptogaphy library
+import cryptography
 
+# Use a symmetric encryption method
+from cryptography.fernet import Fernet
+
+# Load the key
+with open("secret.key", "rb") as key_file: # with, no need to close the file
+    key = key_file.read()
+
+# Create a Fernet object
+cipher = Fernet(key)
+
+# Input secret message
+encrypted_message = input("Secret message: ").encode()
+
+# Decrypt the message
+message = cipher.decrypt(encrypted_message)
+
+# Print the original message
+print("Original message: ", message.decode())
