f92f02389bd3d824f1bbc40bc753be2c4dfba260
djedd1ne <58569774+djedd1ne@users.noreply.github.com>
2024-12-03 23:58:23 +0100

    Generate public key and private key

--- asym.py
+++ asym.py
@@ -1,3 +1,17 @@
 import cryptography
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.asymmetric import rsa
+
+import cryptography
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.asymmetric import rsa
+
+# Generate private key
+private_key = rsa.generate_private_key(
+    public_exponent=65537,
+    key_size=2048,
+    backend=default_backend()
+)
+
+# Generate public key
+public_key = private_key.public_key()
